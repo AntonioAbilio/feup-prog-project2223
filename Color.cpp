54c1c8d45e304ac3a20b60dbe0fae0c83535bf9e
Antonio Abilio <antonioabilio@Antonios-MBP.local>
2023-04-27 15:52:36 +0100

    Color Implementation - Taste the rainbow.

--- Color.cpp
+++ Color.cpp
@@ -2,24 +2,42 @@
 
 namespace prog {
     Color::Color() {
-
+        red_ = 0;
+        green_ = 0;
+        blue_ = 0;
     }
     Color::Color(const Color& other) {
-
+        red_ = other.red();
+        green_ = other.green();
+        blue_ = other.blue();
     }
     Color::Color(rgb_value red, rgb_value green, rgb_value blue) {
+        red_ = red;
+        green_ = green;
+        blue_ = blue;
 
     }
     rgb_value Color::red() const {
-        return 0;
+        return red_;
     }
     rgb_value Color::green() const {
-        return 0;
+        return green_;
     }
     rgb_value Color::blue() const {
-        return 0;
+        return blue_;
     }
 
+    rgb_value& Color::red() {
+        return red_;
+    }
+    rgb_value& Color::green() {
+        return green_;
+    }
+    rgb_value& Color::blue() {
+        return blue_;
+    }
+
+/* 
     // TODO: remove this DUMMY variable once you have appropriate fields for representing colors.
     rgb_value DUMMY_rgb_value = 0; 
 
@@ -31,5 +49,5 @@ namespace prog {
     }
     rgb_value& Color::blue()  {
       return DUMMY_rgb_value;
-    }
+    } */
 }
